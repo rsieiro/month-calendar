6de5c0cdb6e73487cff284b449898aaa0d11f491
rsieiro <rsieiro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-18 14:08:09 +0000

    Lots of code comments!
    git-svn-id: https://plugins.svn.wordpress.org/month-calendar/trunk@334140 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- month-calendar-ajax.php
+++ month-calendar-ajax.php
@@ -1,10 +1,14 @@
 <?php
 
-if (!function_exists('add_action'))
-{
-    require_once("../../../wp-config.php");
-}
-
-mc_reload_calendar();
+	// This will load the basic WP options and make our plugin functions accessible
+	// It's needed because we're calling this PHP directly and not through WP
+	if (!function_exists('add_action'))
+	{
+	    require_once("../../../wp-config.php");
+	}
+	
+	// We simply reload the calendar. The year was informed via a GET parameter
+	// and the HTML output will be the new calendar for the requested year
+	mc_reload_calendar();
 
 ?>
--- month-calendar.js.php
+++ month-calendar.js.php
@@ -1,11 +1,16 @@
 <?php
 
+	// This will load the basic WP options and make our plugin functions accessible
+	// It's needed because we're calling this PHP directly and not through WP
 	if (!function_exists('add_action'))
 	{
     	require_once("../../../wp-config.php");
 	}
 
+	// Let the browser know we're outputting javascript
 	header('Content-type: text/javascript');
+	
+	// This URL will always point to the path our plugin files are located
 	$mc_url = plugins_url() . '/' . str_replace(basename( __FILE__), "", plugin_basename(__FILE__));
 	$mc_file = $mc_url . 'month-calendar-ajax.php';
 
@@ -13,9 +18,11 @@
 
 function mc_reload_calendar(my_year)
 {
+	// Position the loading div over the calendar header, and fade it in
 	jQuery("#mc_calendar_loading").css('top', jQuery("#mc_header").position().top);
 	jQuery("#mc_calendar_loading").fadeIn(400);
 	
+	// The calendar is reloaded via AJAX
 	jQuery.ajax(
 	{
 		type: "GET",
@@ -23,11 +30,15 @@ function mc_reload_calendar(my_year)
 		data: "my_year=" + my_year,
 		success: function(msg)
 		{
+			// Replace the calendar with the data returned by the call
+			// and fade out the loading div
 			jQuery("#mc_calendar").html(msg);
 			jQuery("#mc_calendar_loading").fadeOut(400);
 		},
 		error: function()
 		{
+			// In case of error, we simply return to the previous state
+			// No message is shown
 			jQuery("#mc_calendar_loading").fadeOut(400);
 		}
 	});
